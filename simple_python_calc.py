e8894b5defde0e7585c099798b99421359cc8aeb
ninov0303-beep <ninov0303@gmail.com>
2026-02-16 21:58:16 -0500

    Update simple_python_calc.py
    Add operations for a 3rd number.

--- simple_python_calc.py
+++ simple_python_calc.py
@@ -12,13 +12,16 @@ def divide(x,y):
 number1 = float(input("Enter first number: "))
 operation = input("Choose an operation: ")
 number2 = float(input("Enter second number: "))
+operation = input("Choose an operation: ")
+number3 = float(input("Enter third number: "))
 # operation = input("Choose an operation: ")
 
 if operation == '+':
-    print(add(number1, number2))
+    print(add(number1, number2, number3))
 elif operation == '-':
-    print(subtract(number1, number2))
+    print(subtract(number1, number2, number3))
 elif operation == '*':
-    print(multiply(number1, number2))
+    print(multiply(number1, number2, number3))
 elif operation == '/':
-    print(divide(number1, number2))
+    print(divide(number1, number2, number3))
+
